bb2ef2c5dd1d4febfd9c6a82b40325da7c2cf377
Tommy-Graves <109875296+Tommy-Graves@users.noreply.github.com>
2023-04-12 17:15:24 -0400

    added collision handling to addItem and removeItem functions
    next step is to test both the addItem and removeItem and its collision handling

--- Lab10/hashTable.cpp
+++ Lab10/hashTable.cpp
@@ -19,15 +19,69 @@ void hashTable<T>::setMAX_SIZE(int inVal) {
 
 
 template <class T>
-void hashTable<T>::addItem(T* inVal) {	// currently works but we need at collision handling
+void hashTable<T>::addItem(T* inVal) {	// address represents the index of where inVal will be placed within the array
+	if (items == MAX_SIZE) {
+		// throw overflow error
+	}
+	
 	string sku_str = string(*inVal);
 	int address = hash(sku_str);
-	arr[address] = inVal;
+
+	if (arr[address] == nullptr) {	// covers case where nothing is stored at the hash value
+		arr[address] = inVal;
+		items++;
+	}
+	else {	// covers collisions
+		while (arr[address] != nullptr) {	// iterates address to next open spot within the array
+			if (address < MAX_SIZE) {
+				adress++;
+			}
+			else if (address == MAX_SIZE) {	// resets address to front of array when it iterates to end
+				address = 0;
+			}
+		}
+		arr[address] = inVal;
+		items++;
+	}
+	return;
 }
 
 template <class T>
-T* hashTable<T>::removeItem(T*) {
+T* hashTable<T>::removeItem(T* inVal) {
+	if (items == 0) {
+		// throw underflow exception
+	}
 
+	T* ret = nullptr;
+	string sku_str = string(*inVal);
+	int address = hash(sku_str);
+	int spotsChecked = 0;
+
+	if (*arr[address] == *inVal) {	// item was placed with no collision and is found instantly
+		ret = arr[address];
+		arr[address] = nullptr;	// might cause ret to turn into nullptr, consider changing prev line to 'ret = inVal'
+		return ret;
+	}
+	else {
+		while (*arr[address] != *inVal && spotsChecked < MAX_SIZE) {// iterates until arr[address] == inVal or each spot is checked
+			if (address < MAX_SIZE) {
+				address++;
+				spotsChecked++;
+			}
+			else if (address == MAX_SIZE) {	// resets address to front of array after it iterates till end
+				address = 0;
+				spotsChecked++;
+			}
+		}
+		if (*arr[address] == *inVal) {
+			ret = arr[address];
+			arr[address] = nullptr;
+			return ret;
+		}
+		else {
+			return nullptr;
+		}
+	}
 }
 
 template <class T>
